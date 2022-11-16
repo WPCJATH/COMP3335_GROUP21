77f6f3f7d3136f38cbc53f569354d95d549ca74c
Eric Wang <mdwxz@icloud.com>
2022-11-16 20:30:57 +0800

    Apache Config - Ensure https/TSLv1.3 MySQL Config - Auto-changed to caching_sha2_password

--- LAMP_STACK/dump/myDb.sql
+++ LAMP_STACK/dump/myDb.sql
@@ -1,16 +1,17 @@
-SET time_zone = "+08:00";
-
 CREATE TABLE `Person` (
                           `id` int NOT NULL,
                           `name` varchar(20) NOT NULL,
                           `age` int
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+);
 
 
 INSERT INTO `Person` (`id`, `name`,`age`) VALUES
                                               (1, 'William',30),
                                               (2, 'Marc',17),
                                               (3, 'John',55);
+
+ALTER USER 'root'@'localhost' IDENTIFIED WITH caching_sha2_password BY 'test';
+FLUSH PRIVILEGES;
 
 
 
