2a59157abe623d1a59b9f3754e83a483e7f9b255
AC-Amiya233 <44330556+AC-Amiya233@users.noreply.github.com>
2022-11-20 05:04:32 +0800

    Create encry_decry.php
    AES encryption.php file including how to encrypt and decrypt. Use it when you insert into the database. A user must maintain the information of 1. ciphering, 2. key, 3. iv, 4. options (default as 0)

---
+++ LAMP_STACK/www/encry_decry.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php
+function determine_param($ciphering) {
+    $key_length = 16;
+    switch ($ciphering) {
+        case "AES-128-CBC":
+            echo "AES-128-CBC";
+            $key_length = 16;
+            break;
+        case "AES-128-CTR":
+            echo "AES-128-CTR";
+            $key_length = 16;
+            break;
+        case "AES-196-CBC":
+            echo "AES-196-CBC";
+            $key_length = 24;
+            break;
+        case "AES-256-CBC":
+            echo "AES-256-CBC";
+            $key_length = 32;
+            break;
+        default:
+            $key_length = 64;
+    }
+    $iv_length = openssl_cipher_iv_length($ciphering);
+    $iv = openssl_random_pseudo_bytes($iv_length);
+    $key = openssl_random_pseudo_bytes($key_length);
+    $options = 0;
+    return [$key, $iv, $options];
+}
+
+// echo var_dump(openssl_get_cipher_methods());
+$ciphering = "AES-256-CBC";
+[$key, $iv, $options] = determine_param($ciphering);
+$content = "plain text";
+echo "key: " . $key . "iv: " . $iv . "options: " . $options;
+
+echo "original content: " . $content . "\n";
+$encryption_text = openssl_encrypt($content, $ciphering, $key, $options, $iv);
+echo "encryption: " . $encryption_text . "\n";
+
+$decryption_text = openssl_decrypt($encryption_text, $ciphering, $key, $options, $iv);
+echo "decryption: " . $decryption_text . "\n";
+?>
+
+</body>
+</html>
